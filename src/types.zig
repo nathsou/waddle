c0271dd9653e851566689cce865993ac57ab8497
Nathan Soufflet <nathsoufflet@gmail.com>
2026-01-25 12:57:23 +0100

    Define WASM Core 1.0 types

---
+++ src/types.zig
@@ -0,0 +1,331 @@
+// WASM Core 1.0 types
+
+pub const Byte = u8;
+
+// Indices
+pub const TypeIndex = u32;
+pub const FuncIndex = u32;
+pub const TableIndex = u32;
+pub const MemIndex = u32;
+pub const GlobalIndex = u32;
+pub const LocalIndex = u32;
+pub const LabelIndex = u32;
+
+pub const Name = []const u8;
+
+pub const ValType = enum {
+    i32,
+    i64,
+    f32,
+    f64,
+};
+
+pub const ResultType = []ValType;
+
+pub const BlockType = union(enum) {
+    empty,
+    val_type: ValType,
+};
+
+pub const FuncType = struct {
+    params: []ValType,
+    results: []ValType,
+};
+
+pub const Limits = struct {
+    min: u32,
+    max: ?u32,
+};
+
+pub const MemType = struct {
+    limits: Limits,
+};
+
+pub const ElemType = enum {
+    func_ref,
+};
+
+pub const TableType = struct {
+    elem_type: ElemType,
+    limits: Limits,
+};
+
+pub const GlobalType = struct {
+    val_type: ValType,
+    mutable: bool,
+};
+
+pub const ImportDesc = union(enum) {
+    func: FuncIndex,
+    table: TableType,
+    mem: MemType,
+    global: GlobalType,
+};
+
+pub const MemoryInstrArg = struct { alignment: u32, offset: u32 };
+
+pub const Instr = union(enum) {
+    // Control instructions
+    unreachable_op,
+    nop,
+    block: struct { block_type: BlockType, instrs: []Instr },
+    loop: struct { block_type: BlockType, instrs: []Instr },
+    if_op: struct { block_type: BlockType, then_instrs: []Instr, else_instrs: []Instr },
+    br: LabelIndex,
+    br_if: LabelIndex,
+    br_table: struct { label_indices: []LabelIndex, default_idx: LabelIndex },
+    return_op,
+    call: FuncIndex,
+    call_indirect: struct { type_idx: TypeIndex, table_idx: TableIndex },
+
+    // Parametric instructions
+    drop,
+    select,
+
+    // Variable instructions
+    local_get: LocalIndex,
+    local_set: LocalIndex,
+    local_tee: LocalIndex,
+    global_get: GlobalIndex,
+    global_set: GlobalIndex,
+
+    // Memory instructions
+    i32_load: MemoryInstrArg,
+    i64_load: MemoryInstrArg,
+    f32_load: MemoryInstrArg,
+    f64_load: MemoryInstrArg,
+    i32_load8_s: MemoryInstrArg,
+    i32_load8_u: MemoryInstrArg,
+    i32_load16_s: MemoryInstrArg,
+    i32_load16_u: MemoryInstrArg,
+    i64_load8_s: MemoryInstrArg,
+    i64_load8_u: MemoryInstrArg,
+    i64_load16_s: MemoryInstrArg,
+    i64_load16_u: MemoryInstrArg,
+    i64_load32_s: MemoryInstrArg,
+    i64_load32_u: MemoryInstrArg,
+    i32_store: MemoryInstrArg,
+    i64_store: MemoryInstrArg,
+    f32_store: MemoryInstrArg,
+    f64_store: MemoryInstrArg,
+    i32_store8: MemoryInstrArg,
+    i32_store16: MemoryInstrArg,
+    i64_store8: MemoryInstrArg,
+    i64_store16: MemoryInstrArg,
+    i64_store32: MemoryInstrArg,
+    memory_size: MemIndex,
+    memory_grow: MemIndex,
+
+    // Numeric instructions
+    i32_const: i32,
+    i64_const: i64,
+    f32_const: f32,
+    f64_const: f64,
+    i32_eqz,
+    i32_eq,
+    i32_ne,
+    i32_lt_s,
+    i32_lt_u,
+    i32_gt_s,
+    i32_gt_u,
+    i32_le_s,
+    i32_le_u,
+    i32_ge_s,
+    i32_ge_u,
+    i64_eqz,
+    i64_eq,
+    i64_ne,
+    i64_lt_s,
+    i64_lt_u,
+    i64_gt_s,
+    i64_gt_u,
+    i64_le_s,
+    i64_le_u,
+    i64_ge_s,
+    i64_ge_u,
+    f32_eq,
+    f32_ne,
+    f32_lt,
+    f32_gt,
+    f32_le,
+    f32_ge,
+    f64_eq,
+    f64_ne,
+    f64_lt,
+    f64_gt,
+    f64_le,
+    f64_ge,
+    i32_clz,
+    i32_ctz,
+    i32_popcnt,
+    i32_add,
+    i32_sub,
+    i32_mul,
+    i32_div_s,
+    i32_div_u,
+    i32_rem_s,
+    i32_rem_u,
+    i32_and,
+    i32_or,
+    i32_xor,
+    i32_shl,
+    i32_shr_s,
+    i32_shr_u,
+    i32_rotl,
+    i32_rotr,
+    i64_clz,
+    i64_ctz,
+    i64_popcnt,
+    i64_add,
+    i64_sub,
+    i64_mul,
+    i64_div_s,
+    i64_div_u,
+    i64_rem_s,
+    i64_rem_u,
+    i64_and,
+    i64_or,
+    i64_xor,
+    i64_shl,
+    i64_shr_s,
+    i64_shr_u,
+    i64_rotl,
+    i64_rotr,
+    f32_abs,
+    f32_neg,
+    f32_ceil,
+    f32_floor,
+    f32_trunc,
+    f32_nearest,
+    f32_sqrt,
+    f32_add,
+    f32_sub,
+    f32_mul,
+    f32_div,
+    f32_min,
+    f32_max,
+    f32_copysign,
+    f64_abs,
+    f64_neg,
+    f64_ceil,
+    f64_floor,
+    f64_trunc,
+    f64_nearest,
+    f64_sqrt,
+    f64_add,
+    f64_sub,
+    f64_mul,
+    f64_div,
+    f64_min,
+    f64_max,
+    f64_copysign,
+    i32_wrap_i64,
+    i32_trunc_f32_s,
+    i32_trunc_f32_u,
+    i32_trunc_f64_s,
+    i32_trunc_f64_u,
+    i64_extend_i32_s,
+    i64_extend_i32_u,
+    i64_trunc_f32_s,
+    i64_trunc_f32_u,
+    i64_trunc_f64_s,
+    i64_trunc_f64_u,
+    f32_convert_i32_s,
+    f32_convert_i32_u,
+    f32_convert_i64_s,
+    f32_convert_i64_u,
+    f32_demote_f64,
+    f64_convert_i32_s,
+    f64_convert_i32_u,
+    f64_convert_i64_s,
+    f64_convert_i64_u,
+    f64_promote_f32,
+    i32_reinterpret_f32,
+    i64_reinterpret_f64,
+    f32_reinterpret_i32,
+    f64_reinterpret_i64,
+};
+
+pub const Expr = []Instr;
+
+pub const CustomSection = struct {
+    name: Name,
+    data: []const Byte,
+};
+
+pub const TypeSection = struct {
+    types: []FuncType,
+};
+
+pub const Import = struct {
+    module: Name,
+    name: Name,
+    desc: ImportDesc,
+};
+
+pub const Global = struct {
+    type: GlobalType,
+    init: Expr,
+};
+
+pub const ExportDesc = union(enum) {
+    func: FuncIndex,
+    table: TableIndex,
+    mem: MemIndex,
+    global: GlobalIndex,
+};
+
+pub const Export = struct {
+    name: Name,
+    desc: ExportDesc,
+};
+
+pub const Elem = struct {
+    table: TableIndex,
+    offset: Expr,
+    init: []FuncIndex,
+};
+
+pub const Locals = struct {
+    count: u32,
+    type: ValType,
+};
+
+pub const Func = struct {
+    locals: []Locals,
+    body: Expr,
+};
+
+pub const Code = Func;
+
+pub const Data = struct {
+    mem: MemIndex,
+    offset: Expr,
+    init: []const Byte,
+};
+
+pub const ImportSection = []Import;
+pub const FunctionSection = []TypeIndex;
+pub const TableSection = []TableType;
+pub const MemorySection = []MemType;
+pub const GlobalSection = []Global;
+pub const ExportSection = []Export;
+pub const StartSection = FuncIndex;
+pub const ElementSection = []Elem;
+pub const CodeSection = []Code;
+pub const DataSection = []Data;
+
+pub const Module = struct {
+    custom: []CustomSection,
+    types: TypeSection,
+    imports: ImportSection,
+    functions: FunctionSection,
+    tables: TableSection,
+    memories: MemorySection,
+    globals: GlobalSection,
+    exports: ExportSection,
+    start: ?StartSection,
+    elements: ElementSection,
+    codes: CodeSection,
+    data: DataSection,
+};
